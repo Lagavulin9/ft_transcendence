befb9df426eed195c0d3269d9c13f2577758169d
jinholee <jinholee@student.42seoul.kr>
2023-06-02 20:16:55 +0900

    feat:nest uses .env

--- srcs/requirements/nestjs/nest-app/src/app.module.ts
+++ srcs/requirements/nestjs/nest-app/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({isGlobal: true}),
+            TypeOrmModule.forRoot(typeORMConfig),
+          ],
   controllers: [AppController],
   providers: [AppService],
 })
